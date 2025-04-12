a164f0aed3652cf7d305c8c3896f9160b5cb27ed
k7sky <alex.k7sky@gmail.com>
2025-04-12 21:55:54 +0300

    up versions: spring-boot-starter-parent --> 3.4.4, java --> 17

--- src/test/java/com/k7sky/utils4springwebapp/HttpRequestUtilsTest.java
+++ src/test/java/com/k7sky/utils4springwebapp/HttpRequestUtilsTest.java
@@ -7,7 +7,6 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public class HttpRequestUtilsTest {
